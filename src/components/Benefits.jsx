a7cd5152e0b952c8187361981beeb5bf5d1f3d6e
JamesNyangada <jamesedwinjr@hotmail.com>
2024-11-19 19:30:22 +0300

    updated website

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -3,7 +3,7 @@ import Heading from "./Heading";
 import Section from "./Section";
 import Arrow from "../assets/svg/Arrow";
 import { GradientLight } from "./design/Benefits";
-import ClipPath from "../assets/svg/ClipPath";
+import ClipPath from "../../public/assets/svg/ClipPath";
 
 const Benefits = () => {
   return (
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -25,7 +25,7 @@ const Services = () => {
         <div className="relative">
           <div className="relative z-1 flex items-center h-[39rem] mb-5 p-8 border border-n-1/10 rounded-3xl overflow-hidden lg:p-20 xl:h-[46rem]">
             <div className="absolute top-0 left-0 w-full h-full pointer-events-none md:w-3/5 xl:w-auto">
-              <img src="/src/assets/freepik__upload__98418.jpeg"/>
+              <img src="/assets/vrImage.jpeg"/>
             </div>
 
             <div className="relative z-1 max-w-[17rem] ml-auto text-black ">
