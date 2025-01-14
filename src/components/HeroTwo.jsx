5dc62d69f6fee61e5dde718919863dc53897599b
Kahiri28 <kahiriallan@gmail.com>
2025-01-14 13:20:58 +0300

    Changed up a few wordings here and there

--- src/components/HeroTwo.jsx
+++ src/components/HeroTwo.jsx
@@ -142,13 +142,13 @@ const HeroTwo = () => {
         </div>
 
         <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
-        VR TO<b>U</b>RS 
+        <b>Into Ur Property</b> 
         </h1>
 
         <div className="absolute left-0 top-0 z-40 size-full">
           <div className="mt-24 px-5 sm:px-10">
             <h1 className="special-font hero-heading text-blue-100">
-              redefi<b>n</b>ing
+              <b>Breathing Life</b>
             </h1>
 
             <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
@@ -170,7 +170,7 @@ const HeroTwo = () => {
       </div>
 
       <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">
-        VR TO<b>U</b>RS 
+      <b>Into Ur Property</b> 
       </h1>
     </div>
   );
